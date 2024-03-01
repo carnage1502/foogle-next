69aef0b479de4359f3021761eea9a48d1b1ee076
carnage1502 <shivamsanjay152@gmail.com>
2024-03-01 06:39:47 +0530

    update the search header component

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,5 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+import SearchBox from "./SearchBox";
+import { RiSettings3Line } from "react-icons/ri";
+import { TbGridDots } from "react-icons/tb";
+
 const SearchHeader = () => {
-  return <div>SearchHeader</div>;
+  return (
+    <header className="sticky top-0">
+      <div className="flex w-full p-6 items-center justify-between">
+        <Link href="/">
+          <Image
+            alt="logo"
+            src="/foogle.svg"
+            width="120"
+            height="40"
+            priority
+            style={{ width: "auto" }}
+          />
+        </Link>
+        <div className="flex-1">
+          <SearchBox />
+        </div>
+        <div className="hidden md:inline-flex space-x-2">
+          <RiSettings3Line className="bg-transparent hover:bg-gray-700 p-2 text-4xl rounded-full cursor-pointer" />
+          <TbGridDots className="bg-transparent hover:bg-gray-700 p-2 text-4xl rounded-full cursor-pointer" />
+        </div>
+        <button className="bg-violet-800 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow ml-2">
+          Sign in
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default SearchHeader;
