e3f759e47b0fdf05c9bcea303bca2a74667bb1a7
carnage1502 <shivamsanjay152@gmail.com>
2024-03-01 13:01:42 +0530

    add loading effects for web and images

---
+++ src/app/search/image/loading.jsx
@@ -0,0 +1,25 @@
+const Loading = () => {
+  return (
+    <div className="p-10 mx-2 lg:pl-52 mx-w-6xl flex sm:space-x-4 flex-col sm:flex-row pb-42">
+      <div className="animate-pulse">
+        <div className="h-48 w-48 mb-4 bg-[#5f6368] rounded-md"></div>
+        <div className="h-2 w-48 mb-2.5 bg-[#5f6368] rounded-md"></div>
+        <div className="h-2 w-48 mb-2.5 bg-[#5f6368] rounded-md"></div>
+      </div>
+      <div className="hidden sm:inline-flex sm:space-x-4">
+        <div className="animate-pulse">
+          <div className="h-48 w-48 mb-4 bg-[#5f6368] rounded-md"></div>
+          <div className="h-2 w-48 mb-2.5 bg-[#5f6368] rounded-md"></div>
+          <div className="h-2 w-48 mb-2.5 bg-[#5f6368] rounded-md"></div>
+        </div>
+        <div className="animate-pulse">
+          <div className="h-48 w-48 mb-4 bg-[#5f6368] rounded-md"></div>
+          <div className="h-2 w-48 mb-2.5 bg-[#5f6368] rounded-md"></div>
+          <div className="h-2 w-48 mb-2.5 bg-[#5f6368] rounded-md"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
